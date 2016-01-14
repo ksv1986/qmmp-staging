75b541adaab47360c10f7d42a81ef8c229ec3216
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-01-14 19:07:40 +0000

    updated Russian translation
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6025 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Ui/skinned/translations/skinned_plugin_ru.ts
+++ src/plugins/Ui/skinned/translations/skinned_plugin_ru.ts
@@ -994,25 +994,25 @@
     <message>
         <location filename="../playlistheader.cpp" line="93"/>
         <source>Alignment</source>
-        <translation type="unfinished"></translation>
+        <translation>Выравнивание</translation>
     </message>
     <message>
         <location filename="../playlistheader.cpp" line="94"/>
         <source>Left</source>
         <comment>alignment</comment>
-        <translation type="unfinished"></translation>
+        <translation>По левому краю</translation>
     </message>
     <message>
         <location filename="../playlistheader.cpp" line="95"/>
         <source>Right</source>
         <comment>alignment</comment>
-        <translation type="unfinished"></translation>
+        <translation>По правому краю</translation>
     </message>
     <message>
         <location filename="../playlistheader.cpp" line="96"/>
         <source>Center</source>
         <comment>alignment</comment>
-        <translation type="unfinished"></translation>
+        <translation>По центру</translation>
     </message>
     <message>
         <location filename="../playlistheader.cpp" line="106"/>

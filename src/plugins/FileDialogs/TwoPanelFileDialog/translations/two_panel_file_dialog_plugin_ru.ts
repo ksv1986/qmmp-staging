bca14e66547daec6e9665fe792a4310c3ace6778
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-03-05 11:37:55 +0000

    updated Russian translation
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6159 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/FileDialogs/TwoPanelFileDialog/translations/two_panel_file_dialog_plugin_ru.ts
+++ src/plugins/FileDialogs/TwoPanelFileDialog/translations/two_panel_file_dialog_plugin_ru.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.0" language="ru">
 <context>
     <name>TwoPanelFileDialog</name>
     <message>
         <location filename="../twopanelfiledialog.ui" line="14"/>
         <source>Add Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить файлы</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="73"/>
         <source>File name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Имя файла:</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="83"/>
         <source>Files of type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Тип файла:</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="120"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Воспроизвести</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="133"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="146"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрыть</translation>
     </message>
 </context>
 <context>
@@ -40,22 +40,22 @@
         <location filename="../twopanelfiledialog.cpp" line="68"/>
         <location filename="../twopanelfiledialog.cpp" line="78"/>
         <source>Two-panel File Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Двухпанельный файловый диалог</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.cpp" line="77"/>
         <source>About Two-panel File Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>О двухпанельном файловом диалоге</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.cpp" line="79"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Разработчик: Илья Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.cpp" line="80"/>
         <source>Based on code from the Qt library</source>
-        <translation type="unfinished"></translation>
+        <translation>Часть кода использована из библиотеки Qt</translation>
     </message>
 </context>
 <context>
@@ -64,23 +64,24 @@
         <location filename="../twopanelfiledialogimpl.cpp" line="322"/>
         <location filename="../twopanelfiledialogimpl.cpp" line="338"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialogimpl.cpp" line="340"/>
         <source>Directories</source>
-        <translation type="unfinished"></translation>
+        <translation>Директории</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialogimpl.cpp" line="351"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Сохранить</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialogimpl.cpp" line="458"/>
         <source>%1 already exists.
 Do you want to replace it?</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 уже существует.
+Заменить?</translation>
     </message>
 </context>
 </TS>

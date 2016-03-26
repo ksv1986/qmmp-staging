9a0cc080b547c2b0b1dc7331c6627fb1668c247b
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2016-03-26 17:08:54 +0000

    updated Ukrainian translations
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6198 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/FileDialogs/TwoPanelFileDialog/translations/two_panel_file_dialog_plugin_uk_UA.ts
+++ src/plugins/FileDialogs/TwoPanelFileDialog/translations/two_panel_file_dialog_plugin_uk_UA.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../twopanelfiledialog.ui" line="14"/>
         <source>Add Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати файли</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="73"/>
         <source>File name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ім&apos;я файлу:</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="83"/>
         <source>Files of type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Тип файлу:</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="120"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Відтворити</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="133"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.ui" line="146"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрити</translation>
     </message>
 </context>
 <context>
@@ -40,22 +40,22 @@
         <location filename="../twopanelfiledialog.cpp" line="68"/>
         <location filename="../twopanelfiledialog.cpp" line="78"/>
         <source>Two-panel File Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Двопанельний файловий діалог</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.cpp" line="77"/>
         <source>About Two-panel File Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Про двопанельний файловий діалог</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.cpp" line="79"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialog.cpp" line="80"/>
         <source>Based on code from the Qt library</source>
-        <translation type="unfinished"></translation>
+        <translation>Базується на коді з бібліотеки Qt</translation>
     </message>
 </context>
 <context>
@@ -64,23 +64,24 @@
         <location filename="../twopanelfiledialogimpl.cpp" line="322"/>
         <location filename="../twopanelfiledialogimpl.cpp" line="338"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialogimpl.cpp" line="340"/>
         <source>Directories</source>
-        <translation type="unfinished"></translation>
+        <translation>Теки</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialogimpl.cpp" line="351"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Зберегти</translation>
     </message>
     <message>
         <location filename="../twopanelfiledialogimpl.cpp" line="458"/>
         <source>%1 already exists.
 Do you want to replace it?</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 вже існує.
+Бажаєте замінити?</translation>
     </message>
 </context>
 </TS>

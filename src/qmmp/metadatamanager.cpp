97780a208f7ae3d5c31641562e1fc1b2818d6704
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-04-26 17:50:24 +0000

    fixed taglib warnings
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6269 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmp/metadatamanager.cpp
+++ src/qmmp/metadatamanager.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009-2015 by Ilya Kotov                                 *
+ *   Copyright (C) 2009-2016 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -183,7 +183,7 @@ QPixmap MetaDataManager::getCover(const QString &url)
     if(model)
     {
         QPixmap pix = model->cover();
-        model->deleteLater();
+        delete model;
         if(!pix.isNull())
             return pix;
     }

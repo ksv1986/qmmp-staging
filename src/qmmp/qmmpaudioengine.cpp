9133a5850fc0a53f564fb6d537eb0a8fd642e30b
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-05-20 17:38:51 +0000

    fixed segmentation fault
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6372 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmp/qmmpaudioengine.cpp
+++ src/qmmp/qmmpaudioengine.cpp
@@ -295,6 +295,7 @@ void QmmpAudioEngine::stop()
     reset();
     while(!m_effects.isEmpty()) //delete effects
         delete m_effects.takeFirst();
+    m_replayGain = 0;
 }
 
 qint64 QmmpAudioEngine::produceSound(unsigned char *data, qint64 size, quint32 brate)

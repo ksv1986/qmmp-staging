18fc48f07b760ccf892db8b7079029dff94b5424
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-05-20 15:58:25 +0000

    updated Russian translation
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6369 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Output/wasapi/translations/wasapi_plugin_ru.ts
+++ src/plugins/Output/wasapi/translations/wasapi_plugin_ru.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../outputwasapifactory.cpp" line="28"/>
         <source>WASAPI Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль WASAPI</translation>
     </message>
     <message>
         <location filename="../outputwasapifactory.cpp" line="52"/>
         <source>About WASAPI Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>О модуле вывода WASAPI</translation>
     </message>
     <message>
         <location filename="../outputwasapifactory.cpp" line="53"/>
         <source>Qmmp WASAPI Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль вывода WASAPI для Qmmp</translation>
     </message>
     <message>
         <location filename="../outputwasapifactory.cpp" line="54"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Разработчик: Илья Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
 </context>
 </TS>

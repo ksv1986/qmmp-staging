27d5535eb77d3a160147e852ffd08a9eced8b333
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-06-30 11:34:41 +0000

    qsui: using Qmmp::configDir() for equalizer presets
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6547 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Ui/qsui/equalizer.cpp
+++ src/plugins/Ui/qsui/equalizer.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2011-2015 by Ilya Kotov                                 *
+ *   Copyright (C) 2011-2016 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -140,7 +140,7 @@ void Equalizer::loadPresets()
 {
     m_presetComboBox->clear();
     //equalizer presets
-    QString preset_path = QDir::homePath() +"/.qmmp/eq15.preset";
+    QString preset_path = Qmmp::configDir() + "q15.preset";
     if(!QFile::exists(preset_path))
         preset_path = ":/qsui/eq15.preset";
     QSettings eq_preset (preset_path, QSettings::IniFormat);
@@ -243,7 +243,7 @@ void Equalizer::savePreset()
 
 void Equalizer::savePresets()
 {
-    QSettings eq_preset (QDir::homePath() +"/.qmmp/eq15.preset", QSettings::IniFormat);
+    QSettings eq_preset (Qmmp::configDir() + "eq15.preset", QSettings::IniFormat);
     eq_preset.clear ();
     for (int i = 0; i < m_presets.size(); ++i)
     {

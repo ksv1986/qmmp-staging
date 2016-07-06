d47ba192a0f882d9d4cbe49b3cb13a82ed261934
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-07-06 05:28:33 +0000

    qsui: fixed equalizer presets file path (Ryota Shimamoto)
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6577 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Ui/qsui/equalizer.cpp
+++ src/plugins/Ui/qsui/equalizer.cpp
@@ -140,7 +140,7 @@ void Equalizer::loadPresets()
 {
     m_presetComboBox->clear();
     //equalizer presets
-    QString preset_path = Qmmp::configDir() + "q15.preset";
+    QString preset_path = Qmmp::configDir() + "eq15.preset";
     if(!QFile::exists(preset_path))
         preset_path = ":/qsui/eq15.preset";
     QSettings eq_preset (preset_path, QSettings::IniFormat);

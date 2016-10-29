cac3631de042f414b461e2d5222fb87ec19a9e95
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-10-29 16:03:32 +0000

    removed unused variable
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6814 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Engines/mplayer/mplayerengine.h
+++ src/plugins/Engines/mplayer/mplayerengine.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008-2015 by Ilya Kotov                                 *
+ *   Copyright (C) 2008-2016 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -64,7 +64,6 @@ private slots:
     void startMplayerProcess();
 
 private:
-    int mplayer_pipe[2];
     QStringList m_args;
     QProcess *m_process;
     int m_bitrate;

b6a767af5273b9dafa9a7da7196cb6a0e684a0d4
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2017-01-05 07:49:17 +0000

    update Ukrainian translation
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@6949 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Output/shout/translations/shout_plugin_uk_UA.ts
+++ src/plugins/Output/shout/translations/shout_plugin_uk_UA.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="uk_UA">
+<TS version="2.1" language="uk_UA">
 <context>
     <name>OutputShoutFactory</name>
     <message>
         <location filename="../outputshoutfactory.cpp" line="41"/>
         <source>Icecast Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль Icecast</translation>
     </message>
     <message>
         <location filename="../outputshoutfactory.cpp" line="66"/>
         <source>About Icecast Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль виведення Icecast</translation>
     </message>
     <message>
         <location filename="../outputshoutfactory.cpp" line="67"/>
         <source>Qmmp Icecast Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль виведення Icecast для Qmmp</translation>
     </message>
     <message>
         <location filename="../outputshoutfactory.cpp" line="68"/>
         <source>Compiled against libshout-%1</source>
-        <translation type="unfinished"></translation>
+        <translation>Зібрано з libshout-%1</translation>
     </message>
     <message>
         <location filename="../outputshoutfactory.cpp" line="69"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
 </context>
 <context>
@@ -34,52 +34,52 @@
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>Connection Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Налаштування з&apos;єднання</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="32"/>
         <source>Host:</source>
-        <translation type="unfinished"></translation>
+        <translation>Хост:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="42"/>
         <source>Port:</source>
-        <translation type="unfinished"></translation>
+        <translation>Порт:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="56"/>
         <source>Mount point:</source>
-        <translation type="unfinished"></translation>
+        <translation>Точка монтування:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="66"/>
         <source>User:</source>
-        <translation type="unfinished"></translation>
+        <translation>Користувач:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="76"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Пароль:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="86"/>
         <source>Quality:</source>
-        <translation type="unfinished"></translation>
+        <translation>Якість:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="106"/>
         <source>Sample rate:</source>
-        <translation type="unfinished"></translation>
+        <translation>Частота дискретизації:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="136"/>
         <source>Public</source>
-        <translation type="unfinished"></translation>
+        <translation>Публічний</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="143"/>
         <source>Hz</source>
-        <translation type="unfinished"></translation>
+        <translation>Гц</translation>
     </message>
 </context>
 </TS>

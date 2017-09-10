0164e4ae3e25ee748c42a95b32dc86d950db78d9
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2017-09-10 11:14:18 +0000

    cleanup
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@7459 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Engines/mplayer/mplayerengine.h
+++ src/plugins/Engines/mplayer/mplayerengine.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008-2016 by Ilya Kotov                                 *
+ *   Copyright (C) 2008-2017 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -29,7 +29,6 @@
 
 class Output;
 class QIDevice;
-class DecoderPhonon;
 class QMenu;
 class QProcess;
 class FileInfo;

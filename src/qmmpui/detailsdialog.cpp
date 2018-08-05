1efc9066c8d03a7a3e86dc8438b7b4236e2bd018
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2018-08-05 09:27:37 +0000

    removed useless code
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@8231 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/detailsdialog.cpp
+++ src/qmmpui/detailsdialog.cpp
@@ -72,11 +72,8 @@ void DetailsDialog:: on_directoryButton_clicked()
     }
     else
         return;
-#ifdef QMMP_WS_X11
-    QProcess::execute("xdg-open", QStringList() << dir_path); //works with lxde
-#else
+
     QDesktopServices::openUrl(QUrl::fromLocalFile(dir_path));
-#endif
 }
 
 void DetailsDialog::on_buttonBox_clicked(QAbstractButton *button)

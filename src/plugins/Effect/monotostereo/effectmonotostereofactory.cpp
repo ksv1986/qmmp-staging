c2b48a1403c8943e4c99cf41139a5a0ebd67eda0
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2018-11-13 21:37:19 +0000

    added mono to stereo converter
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@8424 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ src/plugins/Effect/monotostereo/effectmonotostereofactory.cpp
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *   Copyright (C) 2018 by Ilya Kotov                                      *
+ *   forkotov02@ya.ru                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include <QMessageBox>
+#include <qmmp/qmmp.h>
+#include "effectmonotostereofactory.h"
+#include "monotostereoplugin.h"
+
+const EffectProperties EffectMonoToStereoFactory::properties() const
+{
+    EffectProperties properties;
+    properties.name = tr("Mono to Stereo Converter Plugin");
+    properties.shortName = "monotostereo";
+    properties.hasSettings = false;
+    properties.hasAbout = true;
+    return properties;
+}
+
+Effect *EffectMonoToStereoFactory::create()
+{
+    return new MonoToStereoPlugin();
+}
+
+void EffectMonoToStereoFactory::showSettings(QWidget *parent)
+{
+   Q_UNUSED(parent);
+}
+
+void EffectMonoToStereoFactory::showAbout(QWidget *parent)
+{
+    QMessageBox::about(parent, tr("About Mono to Stereo Converter Plugin"),
+                       tr("Qmmp Mono to Stereo Converter Plugin")+"\n"+
+                       tr("Written by: Ilya Kotov <forkotov02@ya.ru>"));
+}
+
+QString EffectMonoToStereoFactory::translation() const
+{
+    return QLatin1String(":/monotostereo_plugin_");
+}
---
+++ src/plugins/Effect/monotostereo/monotostereo.pro
@@ -0,0 +1,16 @@
+include(../../plugins.pri)
+
+TARGET = $$PLUGINS_PREFIX/Effect/monotostereo
+
+HEADERS += monotostereoplugin.h \
+           effectmonotostereofactory.h
+
+SOURCES += monotostereoplugin.cpp \
+           effectmonotostereofactory.cpp
+
+#RESOURCES = translations/translations.qrc
+
+unix {
+    target.path = $$PLUGIN_DIR/Effect
+    INSTALLS += target
+}

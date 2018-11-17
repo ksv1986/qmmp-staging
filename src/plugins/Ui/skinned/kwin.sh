c4f7fa21ae212330e251b4261ba0169be27bb81c
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2018-11-17 16:22:46 +0000

    added kwin.sh
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@8450 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ src/plugins/Ui/skinned/kwin.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#This script adds kwin rules for qmmp windows
+
+count=`kreadconfig5 --file kwinrulesrc --group General --key count`
+i=1
+found=0;
+
+while [ $i -le $count ];
+do
+    desc=`kreadconfig5 --file kwinrulesrc --group $i --key Description`
+    if [ $desc = "qmmp" ]; then
+        found=1
+        break
+    fi
+    i=$((i+1))
+done
+
+if [ $found = "0" ]; then
+    group=$((count+1))
+    kwriteconfig5 --file kwinrulesrc --group General --key count $((count+1))
+    kwriteconfig5 --file kwinrulesrc --group $group --key Description qmmp
+    kwriteconfig5 --file kwinrulesrc --group $group --key skipswitcher true
+    kwriteconfig5 --file kwinrulesrc --group $group --key skipswitcherrule 2
+    kwriteconfig5 --file kwinrulesrc --group $group --key type 5
+    kwriteconfig5 --file kwinrulesrc --group $group --key typerule 2
+    kwriteconfig5 --file kwinrulesrc --group $group --key types 256
+    kwriteconfig5 --file kwinrulesrc --group $group --key wmclass qmmp
+    kwriteconfig5 --file kwinrulesrc --group $group --key wmclasscomplete false
+    kwriteconfig5 --file kwinrulesrc --group $group --key wmclassmatch 1
+    dbus-send --print-reply --dest=org.kde.KWin /KWin org.kde.KWin.reconfigure
+fi

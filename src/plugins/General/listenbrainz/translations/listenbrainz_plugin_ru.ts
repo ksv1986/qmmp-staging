cf7c75aa0e27ae3041b038d2de7c4723c8485169
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-07-02 21:31:37 +0000

    added empty .ts files
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@8985 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ src/plugins/General/listenbrainz/translations/listenbrainz_plugin_ru.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>ListenBrainzFactory</name>
+    <message>
+        <location filename="../listenbrainzfactory.cpp" line="30"/>
+        <source>ListenBrainz Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../listenbrainzfactory.cpp" line="50"/>
+        <source>About ListenBrainz Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../listenbrainzfactory.cpp" line="51"/>
+        <source>Qmmp ListenBrainz Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../listenbrainzfactory.cpp" line="52"/>
+        <source>This plugin submits listen history to ListenBrainz server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../listenbrainzfactory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>ListenBrainz Plugin Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="29"/>
+        <source>ListenBrainz user token:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

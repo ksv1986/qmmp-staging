6b329458884afe9d04016237825aca2bdfacaf1c
ksv1986 <ksv0x07c2@gmail.com>
2019-11-30 00:30:20 +0300

    Don't auto-revert sort selection and sort list

--- src/qmmpui/playlisttask.cpp
+++ src/qmmpui/playlisttask.cpp
@@ -130,7 +130,7 @@ void PlayListTask::sort(QList<PlayListTrack *> tracks, PlayListModel::SortMode m
     if(isRunning())
         return;
     clear();
-    m_reverted = !m_reverted;
+    m_reverted = false;
     m_sort_mode = mode;
     m_task = SORT;
     m_input_tracks = tracks;
@@ -161,7 +161,7 @@ void PlayListTask::sortSelection(QList<PlayListTrack *> tracks, PlayListModel::S
     if(isRunning())
         return;
     clear();
-    m_reverted = !m_reverted;
+    m_reverted = false;
     m_sort_mode = mode;
     m_task = SORT_SELECTION;
     m_tracks = tracks;

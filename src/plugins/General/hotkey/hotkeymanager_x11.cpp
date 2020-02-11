150d73d4681e099825887fd5af719fa63ce3510c
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-02-11 20:53:38 +0000

    updated copyright
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@9224 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/hotkey/hotkeymanager_x11.cpp
+++ src/plugins/General/hotkey/hotkeymanager_x11.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009-2019 by Ilya Kotov                                 *
+ *   Copyright (C) 2009-2020 by Ilya Kotov                                 *
  *   forkotov02@ya.ru                                                      *
  *                                                                         *
  *   Copyright (C) 2003-2007 by Justin Karneges and Michail Pishchagin     *

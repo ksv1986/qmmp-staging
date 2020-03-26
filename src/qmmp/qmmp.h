2d63faa1f564c4347dcd6be9214091b4ca8d9460
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-03-26 15:23:17 +0000

    declare qAsConst for old Qt versions
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@9312 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmp/qmmp.h
+++ src/qmmp/qmmp.h
@@ -39,6 +39,15 @@
 #define QStringToFileName(s) s.toLocal8Bit().constData()
 #endif
 
+#if (QT_VERSION < QT_VERSION_CHECK(5, 7, 0))
+// this adds const to non-const objects (like std::as_const)
+template <typename T>
+Q_DECL_CONSTEXPR typename std::add_const<T>::type &qAsConst(T &t) noexcept { return t; }
+// prevent rvalue arguments:
+template <typename T>
+void qAsConst(const T &&) = delete;
+#endif
+
 
 /*! @brief The Qmmp class stores global settings and enums.
  * @author Ilya Kotov <forkotov02@ya.ru>

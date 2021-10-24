a686f0557e87a5d638631a87163ee5fbd511dc68
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2021-10-24 19:56:13 +0000

    fixed transport plugins loading
    git-svn-id: http://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@10374 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmp/inputsource.cpp
+++ src/qmmp/inputsource.cpp
@@ -161,7 +161,7 @@ QList<InputSourceFactory *> InputSource::enabledFactories()
     {
         if(m_disabledNames.contains(item->shortName()))
             continue;
-        if(item->decoderFactory())
+        if(item->inputSourceFactory())
             list.append(item->inputSourceFactory());
     }
     return list;
